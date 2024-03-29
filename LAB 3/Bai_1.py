cf4ce192ef9ec378b4fbc9cb10411732cf3f5610
bài tập <ttqanh.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-29 12:30:40 +0700

    thêm file

--- LAB 3/Bai_1.py
+++ LAB 3/Bai_1.py
@@ -1,9 +1,40 @@
-#a      
-n = int(input("Nhập vào một số nguyên dương: "))
-if n <= 0:
-    print("Vui lòng nhập lại số: ")
+n=int(input("nhập vào một số nguyên:"))
 
-sum_a = 0
-for i in range(1, n + 1):
-    sum_a += i
-print("Tổng của dãy 1 + 2 + 3 + ... +", n, "là:", sum_a)
+#cau a
+S = 0
+if n<=0:
+    print("Vui lòng nhập lại!")
+else:
+    for i in range(1,n+1):
+        S+=i
+print("Biểu thức S1 = ",S)
+
+#cau b
+S = 0
+if n<=0:
+    print("Vui lòng nhập lại!")
+else:
+    for i in range(1,n+1):
+        S+= 1/i
+print("Biểu thức S2 = ",S)
+
+#cau c
+import math
+S = 1
+if n<=0:
+    print("Vui lòng nhập lại!")
+else:
+    for i in range(1,n+1):
+        S = S + 1/(math.sqrt(2*i))
+print("Biểu thức S3 = ",S)
+
+#cau d
+sum = 0
+if n<0:
+    print("vui lòng nhập lại!")
+else:
+    for i in range(1,n+1):
+        sum+= ((-1)**(i+1))/i
+print("Biểu thức S4 = ",sum)
+    
+
